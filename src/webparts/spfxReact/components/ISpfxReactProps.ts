58e065a14033c5dee3cd292c835f93b8d081cd6d
hesar <student@CONTOSOLTD.LOCAL>
2018-03-30 12:24:05 +0200

    Initial Commit

---
+++ src/webparts/spfxReact/components/ISpfxReactProps.ts
@@ -0,0 +1,3 @@
+export interface ISpfxReactProps {
+  description: string;
+}
---
+++ src/webparts/spfxReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxReactWebPartStrings' {
+  const strings: ISpfxReactWebPartStrings;
+  export = strings;
+}
